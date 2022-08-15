5c7b6d783e15cf2fb619630cac3ff60df2a6ea2e
Franklinx31 <107899692+Franklinx31@users.noreply.github.com>
2022-08-15 09:25:49 -0400

    Add files via upload

--- FransaShop/js/script.js
+++ FransaShop/js/script.js
@@ -1,26 +1,27 @@
 
 
-/* Mostrar contenido ocultado con hidden-xs */
-var objects = document.getElementsByClassName("hidden-menu");
-for (var obj of objects) {
-    console.log(obj);
-}
-
+/* ocultar y mostrar las opciones en nuestro menu */
 var checkbox = document.getElementById("check");
 checkbox.addEventListener("change", validaCheckbox, false);
 function validaCheckbox()
 {
   var checked = checkbox.checked;
-  if(checked){
+  if(checked) {
     document.getElementById("hidden-menu").style.display = "block";
-document.getElementById("hidden-menu2").style.display = "block";
-document.getElementById("hidden-menu3").style.display = "block";
-document.getElementById("hidden-menu4").style.display = "block";
+    document.getElementById("hidden-menu2").style.display = "block";
+    document.getElementById("hidden-menu3").style.display = "block";
+    document.getElementById("hidden-menu4").style.display = "block";
   }
   else {
   	document.getElementById("hidden-menu").style.display = "none";
-document.getElementById("hidden-menu2").style.display = "none";
-document.getElementById("hidden-menu3").style.display = "none";
-document.getElementById("hidden-menu4").style.display = "none";
+    document.getElementById("hidden-menu2").style.display = "none";
+    document.getElementById("hidden-menu3").style.display = "none";
+    document.getElementById("hidden-menu4").style.display = "none";
   }
 }
+
+
+
+
+
+// mostrar y ocultar submenu con jquery
